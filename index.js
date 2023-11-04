5e0eda3a3c13b63a4bad4a5b9d4249bfa0812192
Jongseo Won <jongseowon@Jongseos-MacBook-Pro.local>
2023-11-04 18:10:22 +0900

    add Get all products by category

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const mongoose = require("mongoose");
 const morgan = require("morgan");
 const authRoute = require("./routes/auth");
 const productRouter = require("./routes/admin");
+const userRouter = require("./routes/user");
 
 const PORT = process.env.PORT;
 const app = express();
@@ -19,6 +20,7 @@ app.use(
 );
 
 app.use(authRoute);
+app.use(userRouter);
 app.use(productRouter);
 
 mongoose
--- routes/admin.js
+++ routes/admin.js
@@ -3,7 +3,6 @@ const Product = require("../models/product");
 const { admin } = require("../middlewares/admin");
 
 const productRouter = express.Router();
-
 productRouter.get("/admin/categories", (req, res, next) => {
   const productCategories = [
     "Mobiles",
@@ -17,6 +16,7 @@ productRouter.get("/admin/categories", (req, res, next) => {
     categories: productCategories,
   });
 });
+
 productRouter.get("/admin/products", async (req, res) => {
   try {
     const products = await Product.find();
@@ -29,6 +29,28 @@ productRouter.get("/admin/products", async (req, res) => {
     return res.status(500).json({ error: error.message });
   }
 });
+
+productRouter.delete("/admin/delete-product", admin, async (req, res, next) => {
+  try {
+    const { id } = req.body;
+    const product = await Product.findById(id);
+
+    if (product == null) {
+      return res.status(400).json({ msg: "The Product is NOT found" });
+    }
+
+    const isDelete = await product.delete();
+    console.log("isDelete: ", isDelete);
+
+    console.log("product: ", product);
+
+    return res.json(product);
+  } catch (error) {
+    console.log("error.message: ", error.message);
+    return res.status(500).json({ error: error.message });
+  }
+});
+
 productRouter.post("/admin/add-product", admin, async (req, res) => {
   try {
     const { name, description, price, quantity, category, images } = req.body;
