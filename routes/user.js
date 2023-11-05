32fa4cc8536ef7d0ac83441eede9cc3f89542322
Jongseo Won <jongseowon@Jongseos-MacBook-Pro.local>
2023-11-05 16:39:44 +0900

    add Post add to cart

--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,21 @@
 const express = require("express");
 const User = require("../models/user");
 const { Product } = require("../models/product");
+const { auth } = require("../middlewares/auth");
 
 const userRouter = express.Router();
 
-userRouter.post("/api/add-cart", async (req, res) => {
+// userRouter.get('/api/cart', auth , async (req,res) => {
+//  try {
+//  const user = await User.findById(req.userId);
+
+//  }catch(error) {
+//  console.log('error.message: ', error.message);
+//   return res.status(500).json({ error: error.message });
+//  }
+// })
+
+userRouter.post("/api/add-to-cart", auth, async (req, res) => {
   try {
     const { userId } = req;
     const { productId, quantity } = req.body;
@@ -13,26 +24,40 @@ userRouter.post("/api/add-cart", async (req, res) => {
     console.log("productId: ", productId);
     console.log("quantity: ", quantity);
 
-    let user = User.findById({ _id: userId });
+    let user = await User.findById({ _id: userId });
 
     console.log("user: ", user);
 
-    const product = Product.findById({ _id: productId });
+    const product = await Product.findById({ _id: productId });
 
     if (!product) {
       return res.status(400).json({ msg: "The Product is Not Found." });
     }
 
-    const cartMap = {
-      product,
-      quantity,
-    };
+    if (user.cart.length == 0) {
+      user.cart.push({
+        product,
+        quantity: quantity,
+      });
+    } else {
+      let isProductFound = false;
+      for (let i = 0; i < user.cart.length; i++) {
+        if (user.cart[i].product._id.equals(product._id)) {
+          user.cart[i].quantity++;
+          isProductFound = true;
+          break;
+        }
+      }
 
-    user.cart.push(cartMap);
+      if (!isProductFound) {
+        user.cart.push({
+          product,
+          quantity: quantity,
+        });
+      }
+    }
 
     user = await user.save();
-
-    console.log("user: ", user);
 
     return res.status(200).json(user);
   } catch (error) {
