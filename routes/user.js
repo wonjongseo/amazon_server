2960a302bfea0857c9c87362b311ffc73de347ef
Jongseo Won <jongseowon@Jongseos-MacBook-Pro.local>
2023-11-07 22:42:43 +0900

    1. add save address 2. add update user profile

--- routes/user.js
+++ routes/user.js
@@ -2,8 +2,60 @@ const express = require("express");
 const User = require("../models/user");
 const { Product } = require("../models/product");
 const { auth } = require("../middlewares/auth");
-
+const bcryptjs = require("bcryptjs");
 const userRouter = express.Router();
+
+userRouter.put("/api/update", auth, async (req, res) => {
+  try {
+    const { userId } = req;
+
+    const { body } = req;
+    let user = await User.findById({ _id: userId });
+    if (body.name != null) {
+      user.name = body.name;
+    }
+    if (body.address != null) {
+      user.address = body.address;
+    }
+    if (body.newPassword != null) {
+      const isCorrectPassword = await bcryptjs.compare(
+        user.password,
+        body.oldPassword
+      );
+      if (!isCorrectPassword) {
+        return res.status(400).json({ msg: "The Password is not Correct!" });
+      } else {
+        const newHashedPassowrd = await bcryptjs.hash(body.newPassword, 8);
+        user.password = newHashedPassowrd;
+      }
+    }
+    user = await user.save();
+
+    return res.status(200).json(user);
+  } catch (error) {
+    console.log("error.message: ", error.message);
+    return res.status(500).json({ error: error.message });
+  }
+});
+
+userRouter.post("/api/address", auth, async (req, res) => {
+  try {
+    const { userId } = req;
+    const { address } = req.body;
+
+    const user = await User.findByIdAndUpdate(
+      { _id: userId },
+      {
+        address,
+      }
+    );
+
+    return res.status(200).json(user);
+  } catch (error) {
+    console.log("error.message: ", error.message);
+    return res.status(500).json({ error: error.message });
+  }
+});
 
 userRouter.delete(
   "/api/remove-from-cart/:productId",
